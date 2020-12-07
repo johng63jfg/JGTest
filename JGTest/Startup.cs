275a177cbbca0cf1a8623d61f804ac93cbc14060
JohnG <mrgappdev.gmail.com>
2020-12-07 12:47:31 -0500

    test

--- JGTest/Startup.cs
+++ JGTest/Startup.cs
@@ -32,7 +32,7 @@ namespace JGTest
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World again...!");
                 });
             });
         }
